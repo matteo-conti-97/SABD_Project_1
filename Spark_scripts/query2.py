52e3076d1bf6acf0b24d623541b7cef095c58ddb
Matteo Conti <matteo.conti.977@gmail.com>
2024-05-22 01:14:33 +0200

    Bug fix mongo password docker compose, try spark cache

--- Spark_scripts/query2.py
+++ Spark_scripts/query2.py
@@ -10,35 +10,29 @@ spark = SparkSession.builder \
 
 #prima parte
 start = time.time()
+
 df = spark.read.parquet("hdfs://namenode:8020/disk_data_filtered.parquet")
-df = df.drop("serial_number", "s9_power_on_hours")
+df = df.drop("serial_number", "s9_power_on_hours","date")
 df.cache()
 df1 = df.groupBy("model").agg(sum("failure").alias("failures"))
 df1 = df1.orderBy("failures", ascending=False).limit(10)
+df.show()
+
 end = time.time()
-df1.show()
-print("Execution time: " + str(end - start))
+print("Execution time: first part " + str(end - start))
 
 #seconda parte
-df2 = df.groupBy("vault_id").sum("failure")
-df2.show()
-df2.orderBy("sum(failure)", ascending=False).show()
-df4 = df
+
+df2 = df.groupBy("vault_id").agg(sum("failure").alias("failures")) #AGG a quanto pare non era un bug e non metterlo non permette l'aliasing
+df3 = df.filter(df["failure"] > 0).groupBy("vault_id").agg(collect_set("model").alias("list_of_models"))
+df4 = df3.join(df2, "vault_id").orderBy("failures", ascending=False).limit(10)
 df4.show()
-df3 = df4.filter(df["failure"] > 0).groupBy("vault_id").agg(collect_set("model").alias("list_of_models"))
-df3.show()
-df5 = df3.join(df2, "vault_id").orderBy("failure", ascending=False).limit(10)
-df5.show()
-#df = df.orderBy("failures", ascending=False).limit(10)
+
 end = time.time()
-#df.show()
 print("Execution time: " + str(end - start))
 
-
-#df = df.filter((df["failures"] <= 4) & (df["failures"] >= 2))
-
-df5.write.format("com.mongodb.spark.sql.DefaultSource") \
-  .mode("overwrite") \
-  .save()
+# df5.write.format("com.mongodb.spark.sql.DefaultSource") \
+#   .mode("overwrite") \
+#   .save()
 
 spark.stop()
