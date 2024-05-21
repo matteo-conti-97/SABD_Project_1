d3d41910d301fa726f5a857181b03c23f24bc156
luca <lucafalasca08@gmail.com>
2024-05-21 17:36:37 +0200

    Query 1
    Co-authored-by: Matteo Conti <matteo-conti-97@users.noreply.github.com>

---
+++ Spark_scripts/query1.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder \
+  .appName("HelloWorld") \
+  .config("spark.mongodb.input.uri", "mongodb://spark_user:spark_password@mongo:27017/results.query1") \
+  .config("spark.mongodb.output.uri", "mongodb://spark_user:spark_password@mongo:27017/results.query1") \
+  .getOrCreate()
+
+df = spark.read.parquet("hdfs://namenode:8020/disk_data_filtered.parquet")
+df = df.drop("serial_number", "model", "s9_power_on_hours")
+df = df.withColumn('date', date_format(col('date'), 'dd-MM-yyyy'))
+df = df.groupBy("date", "vault_id").agg(sum("failure").alias("failures"))
+df = df.filter((df["failures"] <= 4) & (df["failures"] >= 2))
+#df.show()
+
+df.write.format("com.mongodb.spark.sql.DefaultSource") \
+  .mode("overwrite") \
+  .save()
+
+spark.stop()
