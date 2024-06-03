34c2ce6aa936a040d1a9095fc459354efbb94107
Matteo Conti <matteo.conti.977@gmail.com>
2024-06-03 18:36:30 +0200

    Added query 3 version with handcratdet quantiles

--- Spark_scripts/query3.py
+++ Spark_scripts/query3.py
@@ -3,6 +3,7 @@ from pyspark.sql.functions import *
 from time import time
 from pyspark.sql.types import StructType, StructField, IntegerType, FloatType, DateType
 from datetime import datetime
+import math
 
 spark = SparkSession.builder \
   .appName("Query3") \
@@ -21,12 +22,14 @@ df.cache()
 failure_1 = df.filter(df["failure"] == 1) \
   .groupBy("serial_number").agg(first("date").alias("dates"), first("s9_power_on_hours").alias("power_on")) \
   .drop("serial_number", "dates") 
-
-failure_1_list = failure_1.approxQuantile("power_on", [0.0, 0.25, 0.5, 0.75, 1.0], 0)
+  
 
 failure_0 = df.filter(df["failure"] == 0) \
   .groupBy("serial_number").agg(first("date").alias("dates"), first("s9_power_on_hours").alias("power_on")) \
   .drop("serial_number", "dates")
+
+
+failure_1_list = failure_1.approxQuantile("power_on", [0.0, 0.25, 0.5, 0.75, 1.0], 0)
 
 failure_0_list = failure_0.approxQuantile("power_on", [0.0, 0.25, 0.5, 0.75, 1.0], 0)
 
@@ -37,11 +40,51 @@ df_final = spark.createDataFrame(
       (0, failure_0_list[0], failure_0_list[1], failure_0_list[2], failure_0_list[3], failure_0_list[4], failure_0.count())
     ], 
     ["failure", "min", "25th_percentile", "50th_percentile", "75th_percentile", "max", "count"])
+
+
+'''
+#Without approxQuantile
+failure_1 = failure_1.orderBy("power_on")
+failure_1.cache()
+cnt=failure_1.count()
+min_f=failure_1.first().power_on
+max_f=failure_1.take(cnt)[-1].power_on
+q25_idx=math.ceil((cnt)*0.25)
+q_75_idx=math.ceil((cnt)*0.75)
+q50_idx=math.ceil((cnt)*0.5)
+q25=failure_1.take(q25_idx)[-1].power_on
+q50=failure_1.take(q50_idx)[-1].power_on
+q75=failure_1.take(q_75_idx)[-1].power_on
+failure_1_list=[min_f,q25,q50,q75,max_f,cnt]
+
+failure_0 = failure_0.orderBy("power_on")
+failure_0.cache()
+cnt=failure_0.count()
+min_f=failure_0.first().power_on
+max_f=failure_0.take(cnt)[-1].power_on
+q25_idx=math.ceil((cnt)*0.25)
+q_75_idx=math.ceil((cnt)*0.75)
+q50_idx=math.ceil((cnt)*0.5)
+q25=failure_0.take(q25_idx)[-1].power_on
+q50=failure_0.take(q50_idx)[-1].power_on
+q75=failure_0.take(q_75_idx)[-1].power_on
+failure_0_list=[min_f,q25,q50,q75,max_f,cnt]
+
+
+df_final = spark.createDataFrame(
+    [
+      (1, failure_1_list[0], failure_1_list[1], failure_1_list[2], failure_1_list[3], failure_1_list[4], failure_1_list[5]), 
+      (0, failure_0_list[0], failure_0_list[1], failure_0_list[2], failure_0_list[3], failure_0_list[4], failure_0_list[5])
+    ], 
+    ["failure", "min", "25th_percentile", "50th_percentile", "75th_percentile", "max", "count"])
+failure_1.unpersist()
+failure_0.unpersist()
+'''
+
+
 df_final.show()
 
 end = time()
-
-
 
 # Save the results of the query
 df_final.write.format("com.mongodb.spark.sql.DefaultSource") \
@@ -83,11 +126,12 @@ failure_1 = df.filter(df["failure"] == 1) \
   .groupBy("serial_number").agg(first("date").alias("dates"), first("s9_power_on_hours").alias("power_on")) \
   .drop("serial_number", "dates")
 
-failure_1_list = failure_1.approxQuantile("power_on", [0.0, 0.25, 0.5, 0.75, 1.0], 0)
-
 failure_0 = df.filter(df["failure"] == 0) \
   .groupBy("serial_number").agg(first("date").alias("dates"), first("s9_power_on_hours").alias("power_on")) \
   .drop("serial_number", "dates")
+
+
+failure_1_list = failure_1.approxQuantile("power_on", [0.0, 0.25, 0.5, 0.75, 1.0], 0)
 
 failure_0_list = failure_0.approxQuantile("power_on", [0.0, 0.25, 0.5, 0.75, 1.0], 0)
 
@@ -98,6 +142,44 @@ df_final = spark.createDataFrame(
       (0, failure_0_list[0], failure_0_list[1], failure_0_list[2], failure_0_list[3], failure_0_list[4], failure_0.count())
     ], 
     ["failure", "min", "25th_percentile", "50th_percentile", "75th_percentile", "max", "count"])
+
+
+'''
+#Without approxQuantile
+failure_1 = failure_1.orderBy("power_on")
+failure_1.cache()
+cnt=failure_1.count()
+min=failure_1.first().power_on
+max=failure_1.take(cnt)[-1].power_on
+q25_idx=int((cnt)*0.25)
+q_75_idx=int((cnt)*0.75)
+q50_idx=int((cnt)*0.5)
+q25=failure_1.take(q25_idx)[-1].power_on
+q50=failure_1.take(q50_idx)[-1].power_on
+q75=failure_1.take(q_75_idx)[-1].power_on
+failure1_list=[min,q25,q50,q75,max,cnt]
+
+failure_0 = failure_0.orderBy("power_on")
+failure_0.cache()
+cnt=failure_0.count()
+min=failure_0.first().power_on
+max=failure_0.take(cnt)[-1].power_on
+q25_idx=int((cnt)*0.25)
+q_75_idx=int((cnt)*0.75)
+q50_idx=int((cnt)*0.5)
+q25=failure_0.take(q25_idx)[-1].power_on
+q50=failure_0.take(q50_idx)[-1].power_on
+q75=failure_0.take(q_75_idx)[-1].power_on
+failure0_list=[min,q25,q50,q75,max,cnt]
+df_final = spark.createDataFrame(
+    [
+      (1, failure_1_list[0], failure_1_list[1], failure_1_list[2], failure_1_list[3], failure_1_list[4], failure_1_list[5]), 
+      (0, failure_0_list[0], failure_0_list[1], failure_0_list[2], failure_0_list[3], failure_0_list[4], failure_0_list[5])
+    ], 
+    ["failure", "min", "25th_percentile", "50th_percentile", "75th_percentile", "max", "count"])
+failure_1.unpersist()
+failure_0.unpersist()
+'''
 
 df_final.show()
 
